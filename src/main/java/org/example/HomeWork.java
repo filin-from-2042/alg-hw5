09a82f4c06c99ce7aa6be8c7033717803499936b
Конов Алексей <AVKonov@sberbank.ru>
2024-10-22 14:55:53 +0300

    Решение Домашнего задания №5 "Стэк (Stack)

--- src/main/java/org/example/HomeWork.java
+++ src/main/java/org/example/HomeWork.java
@@ -1,6 +1,15 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
 public class HomeWork {
+
+    static List<String> OPERATORS = asList("+", "-", "*", "/", "^", "sqr", "sin", "cos", "pow");
 
     /**
      * <h1>Задание 1.</h1>
@@ -16,8 +25,162 @@ public class HomeWork {
      * pow(x,y) = x^y
      */
     double calculate(String expr) {
+        String rpnString = translate(expr);
 
-        return 0;
+        return execute(rpnString);
+    }
+
+    private static Double execute(String rpnString) {
+        String[] rpn = rpnString.split(" ");
+        Deque<Double> values = new LinkedList<>();
+        for (String cur : rpn) {
+            if (isNumber(cur)) {
+                values.push(Double.parseDouble(cur));
+                continue;
+            }
+
+            if (OPERATORS.contains(cur)) {
+                var result = 0d;
+
+                switch (cur) {
+                    case "sqr": {
+                        var opA = values.pop();
+                        result = Math.sqrt(opA);
+                    }
+                    break;
+                    case "cos": {
+                        var opA = values.pop();
+                        result = Math.cos(opA);
+                    }
+                    break;
+                    case "sin": {
+                        var opA = values.pop();
+                        result = Math.sin(opA);
+                    }
+                    break;
+                    case "pow": {
+                        var opB = values.pop();
+                        var opA = values.pop();
+                        result = Math.pow(opA, opB);
+                    }
+                    break;
+                    case "*": {
+                        var opB = values.pop();
+                        var opA = values.pop();
+                        result = opA * opB;
+                    }
+                    break;
+                    case "/": {
+                        var opB = values.pop();
+                        var opA = values.pop();
+                        result = opA / opB;
+                    }
+                    break;
+                    case "+": {
+                        var opB = values.pop();
+                        var opA = values.pop();
+                        result = opA + opB;
+                    }
+                    break;
+                    case "-": {
+                        var opB = values.pop();
+                        var opA = values.pop();
+                        result = opA - opB;
+                    }
+                    break;
+                }
+
+                values.push(result);
+            }
+        }
+        return values.pop();
+    }
+
+    static String translate(String inputString) {
+        String[] input = inputString.split(" ");
+        List<String> output = new ArrayList<>();
+        Deque<String> stack = new LinkedList<>();
+        for (String cur : input) {
+            //Если токен — число, то добавить его в очередь вывода.
+            if (isNumber(cur)) {
+                output.add(cur);
+            }
+
+            //Если токен — оператор op1, то:
+            if (OPERATORS.contains(cur)) {
+                //Пока присутствует на вершине стека токен оператор op2, чей приоритет выше или равен приоритету op1,
+                // и при равенстве приоритетов op1 является левоассоциативным:
+                while (!stack.isEmpty() && !stack.peek().equals("(") && priority(stack.peek()) >= priority(cur)) {
+                    //Переложить op2 из стека в выходную очередь;
+                    output.add(stack.pop().toString());
+                }
+                //Положить op1 в стек.
+                stack.push(cur);
+            }
+            if ((cur.equals("(") || cur.equals(")")) && stack.peek() != null
+                    && (stack.peek().equals("sin") || stack.peek().equals("cos") || stack.peek().equals("sqr") || stack.peek().equals("pow"))) {
+                continue;
+            }
+            //Если токен — открывающая скобка, то положить его в стек.
+            if (cur.equals("(")) {
+                stack.push(cur);
+            }
+            //Если токен — закрывающая скобка:
+            if (cur.equals(")")) {
+                //Пока токен на вершине стека не открывающая скобка
+                while (!stack.isEmpty() && !stack.peek().equals("(")) {
+                    //Переложить оператор из стека в выходную очередь.
+                    output.add(stack.pop().toString());
+                }
+                //Если стек закончился до того, как был встречен токен открывающая скобка, то в выражении пропущена скобка.
+                if (stack.isEmpty()) {
+                    throw new IllegalArgumentException("Missing '(' in expression");
+                }
+                //Выкинуть открывающую скобку из стека, но не добавлять в очередь вывода.
+                stack.pop();
+            }
+        }
+        //Если больше не осталось токенов на входе:
+        //Пока есть токены операторы в стеке:
+        while (!stack.isEmpty()) {
+            //Если токен оператор на вершине стека — открывающая скобка, то в выражении пропущена скобка.
+            if (stack.peek().equals("(")) {
+                throw new IllegalArgumentException("Missing ')' in expression");
+            }
+            //Переложить оператор из стека в выходную очередь.
+            output.add(stack.pop().toString());
+        }
+
+
+        return String.join(" ", output);
+    }
+
+    private static boolean isNumber(String number) {
+        try {
+            Integer.valueOf(number);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private static int priority(String operator) {
+        switch (operator) {
+            case "sqr":
+            case "sin":
+            case "cos":
+                return 4;
+            case "pow":
+                return 3;
+            case "+":
+            case "-":
+                return 1;
+            case "*":
+            case "/":
+                return 2;
+            default:
+                throw new IllegalArgumentException("Unknown operator");
+        }
     }
 
 }
--- src/test/java/org/example/HomeWorkTest.java
+++ src/test/java/org/example/HomeWorkTest.java
@@ -10,7 +10,30 @@ class HomeWorkTest {
 
     @Test
     void check() {
-        assertEquals(1.1, 1.101, 0.0001);
+        assertEquals("2 3 4 * + 5 - 7 6 * 3 / + 2 3 2 pow * - 5 2 - 2 * +", HomeWork.translate("2 + 3 * 4 - 5 + 7 * 6 / 3 - 2 * pow ( 3 , 2 ) + ( 5 - 2 ) * 2"));
     }
 
+    @Test
+    public void should_calculateSinSuccess() {
+        assertEquals(11.0698d, homeWork.calculate("3 * 4 + sin ( 130 )"), 0.0001);
+        assertEquals(11.0698d, homeWork.calculate("sin ( 130 ) + 3 * 4"), 0.0001);
+    }
+
+    @Test
+    public void should_calculateCosSuccess() {
+        assertEquals(11.6327d, homeWork.calculate("3 * 4 + cos ( 130 )"), 0.0001);
+        assertEquals(11.6327d, homeWork.calculate("cos ( 130 ) + 3 * 4"), 0.0001);
+    }
+
+    @Test
+    public void should_calculateSqrSuccess() {
+        assertEquals(14d, homeWork.calculate("3 * 4 + sqr ( 4 )"), 0.0001);
+        assertEquals(14d, homeWork.calculate("sqr ( 4 ) + 3 * 4"), 0.0001);
+    }
+
+    @Test
+    public void should_calculatePowSuccess() {
+        assertEquals(16d, homeWork.calculate("3 * 4 + pow ( 2 , 2 )"), 0.0001);
+        assertEquals(16d, homeWork.calculate("pow ( 2 , 2 ) + 3 * 4"), 0.0001);
+    }
 }
